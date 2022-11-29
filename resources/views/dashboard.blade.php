deccec0e9f1258d40e478ba184bb13d13c292a8a
Suwas52 <suwasdanuwar18@gmail.com>
2022-11-29 11:28:28 +0545

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
 
 <form action="{{route('logout')}}" method="get">
     <div class="container">
-        <button class="btn btn-danger mx-4 ">Logout</button>
+        <button class="btn btn-danger  ">Logout</button>
     </div>
 </form>
 
